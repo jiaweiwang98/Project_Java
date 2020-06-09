ee710152eefb8e5ae76a66611176820f904cad43
Lisan <lis.anneee@outlook.com>
2020-06-09 15:22:28 +0200

    Week 4 klaar?

--- src/main/java/Kassa.java
+++ src/main/java/Kassa.java
@@ -22,18 +22,63 @@ public class Kassa {
      */
     public void rekenAf(Dienblad klant) {
         Iterator<Artikel> it = klant.getArtikelIterator();
-        while (it.hasNext()) {
+            double gekregenKorting = 0;
+            double kortingsPercentage = 0;
+            double maxKorting = -1;
+            double totalePrijs = 0; // Om mee te testen
+            double aantalArtikelen = 0; // Om mee te testen
+
+            while (it.hasNext()) {
 
             double prijs = it.next().getVerkoopPrijs();
 
             try {
-                klant.getKlant().getBetaalwijze().betaal(prijs);
-                this.hoeveelheidGeldInKassa += prijs;
-            } catch (TeWeinigGeldException e) {
-                System.out.println(klant.getKlant().getVoornaam() + " heeft te weinig geld!");
+
+                //Kortingskaarthouder controle
+                double tempKorting = 0;
+
+
+                if(klant.getKlant() instanceof KortingskaartHouder){
+                    KortingskaartHouder kaartHouder = ((KortingskaartHouder)klant.getKlant());
+                    kortingsPercentage = kaartHouder.geefKortingsPercentage();
+                    tempKorting = prijs * kortingsPercentage;
+                    if(kaartHouder.heeftMaximum())
+                        maxKorting = kaartHouder.geefMaximum();
+                }
+
+                if(maxKorting == -1){
+                    klant.getKlant().getBetaalwijze().betaal(prijs - tempKorting);
+                    this.hoeveelheidGeldInKassa += prijs - tempKorting;
+                } else {
+                        if(gekregenKorting < maxKorting) {
+                            if (gekregenKorting + tempKorting > maxKorting) {
+                                tempKorting = maxKorting - gekregenKorting;
+                                gekregenKorting = maxKorting;
+                                klant.getKlant().getBetaalwijze().betaal(prijs - tempKorting);
+                                this.hoeveelheidGeldInKassa += prijs - tempKorting;
+                            } else {
+                                gekregenKorting += tempKorting;
+                                klant.getKlant().getBetaalwijze().betaal(prijs - tempKorting);
+                                this.hoeveelheidGeldInKassa += prijs - tempKorting;
+                            }
+                        }
+                        else {
+                            klant.getKlant().getBetaalwijze().betaal(prijs);
+                            this.hoeveelheidGeldInKassa += prijs ;
+                        }
+                }
+                totalePrijs += prijs;
             }
+            catch (TeWeinigGeldException e) {
+                    System.out.println(klant.getKlant().getVoornaam() + " heeft te weinig geld!");
+            }
+            aantalArtikelen++;
             this.artikelen++;
         }
+            //Even testen of het wel goed gaat...
+//        if(klant.getKlant() instanceof KortingskaartHouder){
+//            System.out.println(klant.getKlant().getVoornaam() + " heeft " + gekregenKorting + " gebruikt bij " + aantalArtikelen + " producten die totaal " + totalePrijs + " kosten. Klant had " + kortingsPercentage + " korting");
+//        }
     }
 
     /**
